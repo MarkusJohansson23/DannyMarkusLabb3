a248d0fd01772016c09b1f6dfd88ba19f544cc81
MarkusJohansson97@outlook.com <MarkusJohansson97@outlook.com>
2021-05-11 14:30:57 +0200

    merge resolved

--- DannyMarkusLabb3/PlaylistForm.cs
+++ DannyMarkusLabb3/PlaylistForm.cs
@@ -21,17 +21,50 @@ namespace DannyMarkusLabb3
 
         private void PlaylistForm_Load(object sender, EventArgs e)
         {
-            using (var db = new everyloopContext())
-            {
-                var playlists = db.Playlists.ToList();
+            //using (var context = new everyloopContext())
+            //{
+            //    try
+            //    {
+            //        dataGridView1.DataSource = context.Playlists.ToList();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show(Convert.ToString(ex));
+            //    }
 
-                DGVPlaylistForm.DataSource = db.Playlists.ToList();
-            }
+            //}
         }
 
         private void ViewPlaylistsbutton_Click(object sender, EventArgs e)
         {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM playlists", sqlConnection);
+                DataTable dtbl = new DataTable();
+                sqlDa.Fill(dtbl);
 
+                DGVPlaylistForm.DataSource = dtbl;
+            }
+            //using (var db = new everyloopContext())
+            //{
+            //    var playlists = db.Playlists
+            //     .Select(p =>
+            //     new Playlist
+            //     {
+            //         PlaylistId = p.PlaylistId,
+            //         Name = p.Name,
+            //     }).ToList();
+
+            //    DataTable dataTable = new DataTable();
+            //    dataTable.Fill();
+
+            //    //dataGridView1.DataSource = playlists;
+            //    //foreach(var p in playlists)
+            //    //{
+            //    //    dataGridView1.DataSource = p;
+            //    //}
+            //}
         }
     }
 }
