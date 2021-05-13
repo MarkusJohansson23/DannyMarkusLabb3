32627fea88c6d51af1ccae2b9a7cc4997f8f2bed
MarkusJohansson97@outlook.com <MarkusJohansson97@outlook.com>
2021-05-13 12:51:18 +0200

    Artist Form search function added

--- DannyMarkusLabb3/AddArtistForm.cs
+++ DannyMarkusLabb3/AddArtistForm.cs
@@ -21,22 +21,103 @@ namespace DannyMarkusLabb3
 
         private void AddArtistForm_Load(object sender, EventArgs e)
         {
-            //using(var context = new everyloopContext())
-            //{
-            //    try
-            //    {
-            //        dataGridView1.DataSource = context.Artists.ToList();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        MessageBox.Show(Convert.ToString(ex));
-            //    }
-                
-            //}
-            
+            using (var db = new everyloopContext())
+            {
+                var tracks = (from t in db.Tracks
+                              join al in db.Albums
+                              on t.AlbumId equals al.AlbumId
+                              join ar in db.Artists
+                              on al.ArtistId equals ar.ArtistId
+                              join g in db.Genres
+                              on t.GenreId equals g.GenreId
+                              select new
+                              {
+                                  Track = t.Name,
+                                  Album = al.Title,
+                                  Artist = ar.Name,
+                                  Genre = g.Name
+                              }).ToList();
+                try
+                {
+                    DGVArtistForm.DataSource = tracks;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(Convert.ToString(ex));
+                }
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void AddArtistButton_Click(object sender, EventArgs e)
+        {
+            using (var db = new everyloopContext())
+            {
+                var addedArtist = new Artist()
+                {
+                    Name = Convert.ToString(ArtistTextBox)
+                };
+                var addedAlbum = new Album()
+                {
+                    Title = Convert.ToString(AlbumTextBox)
+                };
+                var addedGenre = new Genre()
+                {
+                    Name = Convert.ToString(GenreTextBox)
+                };
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            using (var db = new everyloopContext())
+            {
+                var tracks = (from t in db.Tracks
+                              join al in db.Albums
+                              on t.AlbumId equals al.AlbumId
+                              join ar in db.Artists
+                              on al.ArtistId equals ar.ArtistId
+                              join g in db.Genres
+                              on t.GenreId equals g.GenreId
+                              where ar.Name == ArtistTextBox.Text ||
+                              al.Title == AlbumTextBox.Text ||
+                              g.Name == GenreTextBox.Text
+                              select new
+                              {
+                                  Track = t.Name,
+                                  Album = al.Title,
+                                  Artist = ar.Name,
+                                  Genre = g.Name
+                              }
+                              ).ToList();
+                if (tracks != null)
+                {
+                    if (tracks.Count()<=0)
+                    {
+                        MessageBox.Show("No records found", "No data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+
+                try
+                {
+                    DGVArtistForm.DataSource = tracks;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(Convert.ToString(ex));
+                }
+            }
+        }
+
+        private void UpdateArtistButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RemoveArtistButton_Click(object sender, EventArgs e)
         {
 
         }
--- DannyMarkusLabb3/PlaylistForm.Designer.cs
+++ DannyMarkusLabb3/PlaylistForm.Designer.cs
@@ -30,14 +30,18 @@ namespace DannyMarkusLabb3
         private void InitializeComponent()
         {
             this.DGVPlaylistForm = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.RemovePlaylistButton = new System.Windows.Forms.Button();
+            this.ViewTracksButton = new System.Windows.Forms.Button();
+            this.AddPlaylistButton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.PlaylistNameTextBox = new System.Windows.Forms.TextBox();
             this.ViewPlaylistsbutton = new System.Windows.Forms.Button();
+            this.PlaylistNameLable = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DGVPlaylistForm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // DGVPlaylistForm
@@ -46,60 +50,61 @@ namespace DannyMarkusLabb3
             this.DGVPlaylistForm.Location = new System.Drawing.Point(12, 215);
             this.DGVPlaylistForm.Name = "DGVPlaylistForm";
             this.DGVPlaylistForm.RowTemplate.Height = 25;
-            this.DGVPlaylistForm.Size = new System.Drawing.Size(873, 334);
+            this.DGVPlaylistForm.Size = new System.Drawing.Size(411, 334);
             this.DGVPlaylistForm.TabIndex = 0;
             // 
-            // button1
+            // RemovePlaylistButton
             // 
-            this.button1.Location = new System.Drawing.Point(312, 170);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 39);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.RemovePlaylistButton.Location = new System.Drawing.Point(258, 146);
+            this.RemovePlaylistButton.Name = "RemovePlaylistButton";
+            this.RemovePlaylistButton.Size = new System.Drawing.Size(104, 39);
+            this.RemovePlaylistButton.TabIndex = 1;
+            this.RemovePlaylistButton.Text = "Remove";
+            this.RemovePlaylistButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // ViewTracksButton
             // 
-            this.button2.Location = new System.Drawing.Point(312, 125);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(104, 39);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.ViewTracksButton.Location = new System.Drawing.Point(258, 101);
+            this.ViewTracksButton.Name = "ViewTracksButton";
+            this.ViewTracksButton.Size = new System.Drawing.Size(104, 39);
+            this.ViewTracksButton.TabIndex = 2;
+            this.ViewTracksButton.Text = "View Tracks";
+            this.ViewTracksButton.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // AddPlaylistButton
             // 
-            this.button3.Location = new System.Drawing.Point(312, 80);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(104, 39);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.AddPlaylistButton.Location = new System.Drawing.Point(258, 56);
+            this.AddPlaylistButton.Name = "AddPlaylistButton";
+            this.AddPlaylistButton.Size = new System.Drawing.Size(104, 39);
+            this.AddPlaylistButton.TabIndex = 3;
+            this.AddPlaylistButton.Text = "Add";
+            this.AddPlaylistButton.UseVisualStyleBackColor = true;
+            this.AddPlaylistButton.Click += new System.EventHandler(this.AddPlaylistButton_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(131, 186);
+            this.textBox1.Location = new System.Drawing.Point(382, 56);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 23);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(131, 157);
+            this.textBox2.Location = new System.Drawing.Point(763, 101);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 23);
             this.textBox2.TabIndex = 5;
             // 
-            // textBox3
+            // PlaylistNameTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(131, 128);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 23);
-            this.textBox3.TabIndex = 6;
+            this.PlaylistNameTextBox.Location = new System.Drawing.Point(97, 88);
+            this.PlaylistNameTextBox.Name = "PlaylistNameTextBox";
+            this.PlaylistNameTextBox.Size = new System.Drawing.Size(100, 23);
+            this.PlaylistNameTextBox.TabIndex = 6;
             // 
             // ViewPlaylistsbutton
             // 
-            this.ViewPlaylistsbutton.Location = new System.Drawing.Point(776, 186);
+            this.ViewPlaylistsbutton.Location = new System.Drawing.Point(12, 186);
             this.ViewPlaylistsbutton.Name = "ViewPlaylistsbutton";
             this.ViewPlaylistsbutton.Size = new System.Drawing.Size(109, 23);
             this.ViewPlaylistsbutton.TabIndex = 7;
@@ -107,23 +112,54 @@ namespace DannyMarkusLabb3
             this.ViewPlaylistsbutton.UseVisualStyleBackColor = true;
             this.ViewPlaylistsbutton.Click += new System.EventHandler(this.ViewPlaylistsbutton_Click);
             // 
+            // PlaylistNameLable
+            // 
+            this.PlaylistNameLable.AutoSize = true;
+            this.PlaylistNameLable.Location = new System.Drawing.Point(12, 91);
+            this.PlaylistNameLable.Name = "PlaylistNameLable";
+            this.PlaylistNameLable.Size = new System.Drawing.Size(79, 15);
+            this.PlaylistNameLable.TabIndex = 8;
+            this.PlaylistNameLable.Text = "Playlist Name";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(429, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(456, 334);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(699, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
             // PlaylistForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(897, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.PlaylistNameLable);
             this.Controls.Add(this.ViewPlaylistsbutton);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.PlaylistNameTextBox);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddPlaylistButton);
+            this.Controls.Add(this.ViewTracksButton);
+            this.Controls.Add(this.RemovePlaylistButton);
             this.Controls.Add(this.DGVPlaylistForm);
             this.Name = "PlaylistForm";
             this.Text = "PlaylistForm";
             this.Load += new System.EventHandler(this.PlaylistForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGVPlaylistForm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,12 +168,15 @@ namespace DannyMarkusLabb3
         #endregion
 
         private System.Windows.Forms.DataGridView DGVPlaylistForm;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button RemovePlaylistButton;
+        private System.Windows.Forms.Button ViewTracksButton;
+        private System.Windows.Forms.Button AddPlaylistButton;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox PlaylistNameTextBox;
         private System.Windows.Forms.Button ViewPlaylistsbutton;
+        private System.Windows.Forms.Label PlaylistNameLable;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
     }
 }
